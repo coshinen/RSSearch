51a6a596a1419342586a94b454db5942b256046b
mistydew <mistydew@qq.com>
2017-12-09 20:50:03 +0800

    modify #include "xxx/xxx" and Makefile

--- offline/src/RssReader.cc
+++ offline/src/RssReader.cc
@@ -8,8 +8,8 @@
 #include "Configuration.h"
 
 #include "tinyxml2.h"
-#include "../deps/boost/regex.hpp"
-#include "../deps/simhash/Simhasher.hpp"
+#include "boost/regex.hpp"
+#include "simhash/Simhasher.hpp"
 
 #include <sys/types.h>
 #include <dirent.h>
